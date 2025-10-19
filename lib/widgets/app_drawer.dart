ea37b7d5a59e6d330766c34a47b897880ebb08e0
amjad tamish <amjad.tamish@centralbank.gov.ye>
2025-10-19 11:48:27 +0300

    try to customize drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -10,9 +10,7 @@ class AppDrawer extends StatelessWidget {
         children: [
           // --- Drawer header ---
           UserAccountsDrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.teal,
-            ),
+            decoration: const BoxDecoration(color: Colors.teal),
             currentAccountPicture: const CircleAvatar(
               backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=12'),
             ),
@@ -22,6 +20,46 @@ class AppDrawer extends StatelessWidget {
             ),
             accountEmail: const Text('tamish@gmail.com'),
           ),
+
+          // // --- Custom Drawer header ---
+          // Container(
+          //   color: Colors.teal,
+          //   padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+          //   child: Row(
+          //     children: [
+          //       const CircleAvatar(
+          //         radius: 30,
+          //         backgroundImage: NetworkImage(
+          //           'https://i.pravatar.cc/150?img=12',
+          //         ),
+          //       ),
+          //       const SizedBox(width: 16),
+          //       Expanded(
+          //         child: Column(
+          //           mainAxisAlignment: MainAxisAlignment.center,
+          //           // Vertically center
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           // Align text next to avatar
+          //           children: const [
+          //             Text(
+          //               'Hello, Tamish!',
+          //               style: TextStyle(
+          //                 fontWeight: FontWeight.bold,
+          //                 fontSize: 18,
+          //                 color: Colors.white,
+          //               ),
+          //             ),
+          //             SizedBox(height: 4),
+          //             Text(
+          //               'tamish@gmail.com',
+          //               style: TextStyle(color: Colors.white70),
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
 
           // --- Drawer menu items ---
           ListTile(
@@ -52,10 +90,7 @@ class AppDrawer extends StatelessWidget {
           // --- Logout item ---
           ListTile(
             leading: const Icon(Icons.logout, color: Colors.red),
-            title: const Text(
-              'Logout',
-              style: TextStyle(color: Colors.red),
-            ),
+            title: const Text('Logout', style: TextStyle(color: Colors.red)),
             onTap: () {},
           ),
         ],
